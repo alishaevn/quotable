34ca4c5cffe314a74c0dede494ed61e2f01c75ed
alishaevn <alieva.inc@gmail.com>
2018-04-24 12:59:39 -0700

    removed sample styles

--- src/styles.js
+++ src/styles.js
@@ -8,64 +8,11 @@ const constants = {
 var styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    // backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  listview: {
-    flex: 1,
-  },
-  li: {
-    backgroundColor: '#fff',
-    borderBottomColor: '#eee',
-    borderColor: 'transparent',
-    borderWidth: 1,
-    paddingLeft: 16,
-    paddingTop: 14,
-    paddingBottom: 16,
-  },
-  liContainer: {
-    flex: 2,
-  },
-  liText: {
-    color: '#333',
-    fontSize: 16,
-  },
-  navbar: {
-    alignItems: 'center',
-    backgroundColor: '#fff',
-    borderBottomColor: '#eee',
-    borderColor: 'transparent',
-    borderWidth: 1,
-    justifyContent: 'center',
-    height: 44,
-    flexDirection: 'row'
-  },
-  navbarTitle: {
-    color: '#444',
-    fontSize: 16,
-    fontWeight: "500"
-  },
-  statusbar: {
-    backgroundColor: '#fff',
-    height: 22,
-  },
-  center: {
-    textAlign: 'center',
-  },
-  actionText: {
-    color: '#fff',
-    fontSize: 16,
-    textAlign: 'center',
-  },
-  action: {
-    backgroundColor: constants.actionColor,
-    borderColor: 'transparent',
-    borderWidth: 1,
-    paddingLeft: 16,
-    paddingTop: 14,
-    paddingBottom: 16,
-  },
+  
 })
 
 module.exports = styles
