07c93c9b1723a2239a58bbf1b19f866790ff28b3
alishaevn <alieva.inc@gmail.com>
2018-04-25 11:39:31 -0700

    can create new user with OAuth

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,6 @@
 import React, { Component} from 'react'
-import { Text, View, TextInput } from 'react-native'
-// import firebase from 'firebase'
+import { Text, View, TextInput, Button } from 'react-native'
+import firebase from 'firebase'
 // import firebaseui from 'firebaseui'
 
 
@@ -11,12 +11,68 @@ export default class Login extends Component {
   constructor() {
     super()
     this.state = {
-
+      registering: false
     }
   }
 
+  onLogin = () => {
+  const { email, password } = this.state;
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .catch((error) => {
+      const { code, message } = error;
+      // The message contains the default Firebase string representation of the error
+      if (code === 'auth/wrong-password') {
+        alert('Wrong password.');
+      } else {
+        alert(message);
+      }
+    });
+}
+
+onRegister = () => {
+  const { email, password } = this.state;
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+    .catch((error) => {
+      const { code, message } = error;
+      // The message contains the default Firebase string representation of the error
+      if (code === 'auth/weak-password') {
+        alert('The password is too weak.');
+      } else {
+        alert(message);
+      }
+    });
+}
+
   render() {
-    // console.log('email:', this.state.email)
+   
+    let button;
+    let registerMessage;
+
+    if(this.state.registering) {
+      button = (<Button
+          onPress={() => this.onRegister()}
+          title="Register"
+          accessibilityLabel="Register for Quotable"
+        />)
+        registerMessage = (
+        <Text
+          onPress={() => this.setState({registering: false})}
+        >
+          Click here to Login
+        </Text>)
+    } else {
+      button = (<Button
+          onPress={() => this.onLogin()}
+          title="Login"
+          accessibilityLabel="Log in to Quotable"
+        />)
+      registerMessage = (
+        <Text
+          onPress={() => this.setState({registering: true})}
+        >
+          Click here to register
+        </Text>)
+    }
 
     return(
       <View style={styles.container}>
@@ -35,16 +91,21 @@ export default class Login extends Component {
           value={this.state.password}
         />
 
-        <Text style={styles.text}>Phone Number</Text>
-        <TextInput
-          style={styles.inputField}
-          onChangeText={(phoneNumber) => this.setState({phoneNumber})}
-          value={this.state.phoneNumber}
-        />
+        { button }
+        { registerMessage }
 
       </View>
-      
     )
   }
 
 }
+
+/*
+ Resources:
+ -https://blog.invertase.io/getting-started-with-firebase-authentication-on-react-native-a1ed3d2d6d91
+
+  - https://firebase.google.com/docs/reference/js/firebase.auth.Auth?authuser=0#createUserWithEmailAndPassword
+
+ - https://facebook.github.io/react-native/docs/components-and-apis.html
+  
+ */
