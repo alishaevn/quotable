5e11f36d7687c44b9120bfe840a4718a2b9b0d2a
alishaevn <alieva.inc@gmail.com>
2018-04-25 09:51:04 -0700

    app redirects to login screen without a user object

--- App.js
+++ App.js
@@ -4,18 +4,38 @@ import firebase from 'firebase'
 
 import Home from './src/components/Home'
 import { API_KEY, MESSAGING_SENDER_ID } from './src/config'
+import Login from './src/components/Login'
 
 import * as styles from './src/styles'
 
 export default class App extends Component {
+constructor() {
+    super()
+    this.state = {
+      loading: true
+    }
+  }
+  
+  componentDidMount() {
+    this.authSubscription = firebase.auth().onAuthStateChanged((user) => {
+      this.setState({
+        loading: false,
+        user,
+      })
+    })
+  }
+
+  componentWillUnmount() {
+    this.authSubscription();
+  }
 
   render() {
-    return (
-      <View style={styles.container}>
-        <Home />
-      </View>
-    );
+    
+    if (this.state.loading) return null // The application is loading
+    if (this.state.user) return <Home /> // The user is logged in (not null) so redirect Home
+    return <Login /> // The user is logged out (null) so redirect to the login page
   }
+  
 }
 
 /*
