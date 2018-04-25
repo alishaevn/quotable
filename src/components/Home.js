2b380e1271e97fa90fd2ef9190058e6e4b382c7a
alishaevn <alieva.inc@gmail.com>
2018-04-24 21:45:17 -0700

    component displays first quote in db upon mounting

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,54 @@
 import React, { Component } from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import firebase from 'firebase'
+
+import { firebaseConfig } from '../config'
 
 import * as styles from '../styles'
 
+
 export default class Home extends Component {
+  constructor() {
+    super()
+    this.state = {
+      quote: '',
+      author: ''
+    }
+  }
+  
+  componentDidMount() {
+    const firebaseApp = firebase.initializeApp(firebaseConfig)
+    const firebaseDb = firebaseApp.database().ref()
+
+    firebaseDb.on('value', snap => {
+      const quoteObj =  snap.val()[0]
+      this.setState({
+        quote: quoteObj.quote,
+        author: quoteObj.author
+      })
+    })
+  }
+
+
   render() {
+
     return (
       <View style={styles.container}>
-        <Text>Welcome to Quotable!</Text>
+        <Text>{this.state.quote}</Text>
+        <Text>{this.state.author}</Text>
       </View>
     );
   }
 }
+
+/*
+
+firebase-app - The core firebase client (required).
+firebase-auth - Firebase Authentication (optional).
+firebase-database - The Firebase Realtime Database (optional).
+firebase-firestore - Cloud Firestore (optional).
+firebase-storage - Cloud Storage (optional).
+firebase-messaging - Firebase Cloud Messaging (optional).
+firebase-functions - Cloud Functions for Firebase (optional).
+
+*/
