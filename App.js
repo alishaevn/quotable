7fe53ea1f0ec76aec11665bc14dd2c2a2fdc44e6
alishaevn <alieva.inc@gmail.com>
2018-04-27 11:02:04 -0700

    removed unused code

--- App.js
+++ App.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
 import firebase from 'firebase'
 
 import Home from './src/components/Home'
-import { API_KEY, MESSAGING_SENDER_ID } from './src/config'
 import Login from './src/components/Login'
 
-import * as styles from './src/styles'
 
 export default class App extends Component {
 constructor() {
